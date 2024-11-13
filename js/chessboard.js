efdf27d737d884e556faefa5203c46917d9e0d69
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-11-13 20:54:21 +0200

    chore: precommit for option3

--- js/chessboard.js
+++ js/chessboard.js
@@ -4,73 +4,57 @@ export default {
 };
 
 
-var tiles;
+var tiles = [];
 
 // ****************************
 
 function draw(boardEl) {
 	
 	for (let i = 0; i < 8; i++) {
-		var origBoardEl = boardEl;
 		let rowEl = document.createElement("div");
+		let rowTiles = [];
+		tiles.push(rowTiles)
 		for (let j = 0; j < 8; j++) {
 			let tileEl = document.createElement("div");
 			tileEl.dataset.row = i;
 			tileEl.dataset.col = j;
 			rowEl.appendChild(tileEl);
+			rowTiles.push(tileEl);
 		}
 		boardEl.appendChild(rowEl);
 	}
-
-	tiles = origBoardEl.querySelectorAll("div > div");
 }
 
 function highlight(tileEl) {
-
 	// clear all currently highlighted tiles
 	for (let el of tiles) {
 		el.classList.remove("highlighted");
 	}
 
 	if (tileEl) {
-		let rowEl = tileEl.parentNode;
-		let boardEl = rowEl.parentNode;
-		let tileRowIdx = [ ...boardEl.childNodes ].indexOf(rowEl);
-		let tileColIdx = [ ...rowEl.childNodes ].indexOf(tileEl);
+		let tileRowIdx = Number(tileEl.dataset.row);
+		let tileColIdx = Number(tileEl.dataset.col);
 
 		// highlight in the up-left direction
 		for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j >= 0; i--, j--) {
-			let el = findTile(i,j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
 		}
 		// highlight in the up-right direction
 		for (let i = tileRowIdx, j = tileColIdx; i >= 0 && j < 8; i--, j++) {
-			let el = findTile(i,j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
 		}
 
 		// highlight in the down-left direction
 		for (let i = tileRowIdx, j = tileColIdx; i < 8 && j >= 0; i++, j--) {
-			let el = findTile(i,j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
 		}
 		// highlight in the down-right direction
 		for (let i = tileRowIdx, j = tileColIdx; i < 8 && j < 8; i++, j++) {
-			let el = findTile(i,j);
+			let el = tiles[i][j];
 			el.classList.add("highlighted");
-		}
-	}
-}
-
-function findTile(row,col) {
-	// return document.querySelector(`
-	// 		#board > 
-	// 		div:nth-child(${ row + 1}) >
-	// 		div:nth-child(${ col + 1})
-	// 	`)
-	for (let el of tiles) {
-		if (el.dataset.row == row && el.dataset.col == col) {
-			return el;
 		}
 	}
 }
